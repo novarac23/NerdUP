312ab02039a9be4adcdc0ea35ec66754ae9196cf
Nikola Novakovic <nikola.novakovic9@gmail.com>
2015-03-16 09:14:38 +0100

    Modified git ignore and views

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ActiveRecord::Base
   has_attached_file :nerd, :styles => { :medium => "800x600>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :nerd, :content_type => /\Aimage\/.*\Z/
+  validates :nerd, :attachment_presence => true
+  validates_with AttachmentSizeValidator, :attributes => :nerd, :less_than => 1.megabytes
 
   belongs_to :user
 
